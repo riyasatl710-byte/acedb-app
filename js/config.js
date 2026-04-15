f93f8deb7c9462047cf9585933d44fb66f9a7e89
riyasatl710-byte <riyasatl710@gmail.com>
2026-04-15 18:03:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 ﻿/* ACEDB - config.js */
 const ACEDB_CONFIG = {
   // IMPORTANT: Replace this URL after deploying your Apps Script Web App
-  API_URL: 'YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxO5aacZsOpii7ZjmoJmba1heT1kI9Eau-HjnIazwETOhktbcanMF7xCt4d9ekPQId0/exec',
   APP_NAME: 'ACEDB',
   APP_FULL_NAME: 'Agricultural Contract Employee DataBase',
   VERSION: '1.0.0',
